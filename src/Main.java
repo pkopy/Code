8c4b727dfbd931cd258bae69fcb583a550f9ae9a
pkopy <pawel.kopycki@gmail.com>
2018-05-02 08:12:37 +0200

    Init commit

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,11 @@
 public class Main {
     public static void main(String[] args) {
+
         Code code = new Code("Taaaka ryba jadła grzyyyba","most");
         System.out.println(code.getResult());
+
         Decode decode = new Decode("ÓOSTWORAAPSSVOVMMNYABCGHNO", "most");
-
         System.out.println(decode.getResult());
-
-
 
     }
 }
